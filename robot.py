3a2ba87c76802f1df01e3b6a27bf61b934213c3c
WUXXXXXYI <wxxxxxi@brew.wireless.unimaas.local>
2020-02-12 18:20:02 +0100

    a lot

--- robot.py
+++ robot.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 import numpy as np
+import pygame.gfxdraw as gfxdraw
 
 WHITE  = (255, 255, 255)
 GREEN  = (20, 255, 140)
@@ -9,18 +10,19 @@ RED    = (255, 0, 0)
 PURPLE = (255, 0, 255)
 BLACK  = (0,0,0)
 
+pygame.init()
 background_colour = RED
 (width, height) = (1000, 1000)
 PI = np.pi
 cos = np.cos
 sin = np.sin
-
+decrease_factor = 1
+increase_factor = 1
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Assignment 2')
+font = pygame.font.SysFont("Helvetica", 20)
 # screen.fill(background_colour)
 clock = pygame.time.Clock()
-import jedi
-jedi.preload_module("pygame", "numpy")
 
 class Wall:
   def __init__(self, start_point  ,end_point):
@@ -42,15 +44,48 @@ class Robot:
         # self.rect = pygame.rect.Rect((self.x, self.y, self.width, self.height))
         # pygame.draw.rect(screen, GREEN, self.rect)
         # self.rect = pygame.circ.Rect((self.x, self.y, self.width, self.height))
-        pygame.draw.circle(screen, GREEN, [self.x,self.y], self.radius)
+        # pygame.draw.circle(screen, GREEN, [self.x,self.y], self.radius)
+        gfxdraw.aacircle(screen, self.x, self.y, self.radius, GREEN)
+        gfxdraw.filled_circle(screen, self.x, self.y, self.radius, GREEN)
 
     def draw_direction(self):
         pygame.draw.line(screen, RED, (self.x, self.y), \
                          (self.x + self.radius * cos(self.angle),
                          self.y + self.radius * sin(self.angle)), 2)
 
+    def speedup_left(self):
+        self.left_velocity += increase_factor
+
+    def slowdown_left(self):
+        self.left_velocity -= decrease_factor
+
+    def speedup_right(self):
+        self.right_velocity += increase_factor
+
+    def slowdown_right(self):
+        self.right_velocity -= decrease_factor
+
+    def speedup_both(self):
+        self.left_velocity += increase_factor
+        self.right_velocity += increase_factor
+
+    def slowdown_both(self):
+        self.right_velocity -= decrease_factor
+        self.right_velocity -= decrease_factor
+
+    def stop_both(self):
+        self.right_velocity = self.left_velocity = 0
+
     def get_velocity(self):
         self.velocity = (self.left_velocity + self.right_velocity) / 2
+
+    def distance_from_ICC(self):
+        return self.radius * ( (self.right_velocity + self.left_velocity) / (self.right_velocity - self.left_velocity) )
+
+    def omega(self):
+        omega = (self.right_velocity - self.left_velocity) / (2 * self.radius)
+        return omega
+
     def rotate(self, clockwise = True):
         if clockwise:
             self.angle += PI / 20
@@ -66,6 +101,12 @@ class Robot:
         # pygame.draw.rect(screen, RED, rect)
         self.draw()
 
+    def message_display(self):
+        messages = [f"l velocity: {self.left_velocity}", \
+                    f"r velocity: {self.right_velocity}"]
+        message = "\n".join(messages)
+        text = font.render(message, True ,(0, 128, 0))
+        return text
 
 block = Robot(100, 100, 5, 5, 30)
 wall=Wall((100,200),(200,900))
@@ -78,14 +119,24 @@ def gameloop():
           if event.type == pygame.QUIT:
               loopExit = False
           if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-              t = t+1
-            elif event.key == pygame.K_w:
-              block.rotate()
-            elif event.key == pygame.K_s:
-              block.rotate(False)
-            elif event.key == pygame.K_ESCAPE:
-              loopExit = False
+              if event.key == pygame.K_UP:
+                  t = t+1
+              elif event.key == pygame.K_w:
+                  block.rotate()
+              elif event.key == pygame.K_s:
+                  block.rotate(False)
+              elif event.key == pygame.K_o:
+                  block.speedup_left()
+              elif event.key == pygame.K_l:
+                  block.slowdown_left() # decrement of left wheel
+              elif event.key == pygame.K_x:
+                  block.stop_both() # zero both wheel speed
+              elif event.key == pygame.K_t:
+                  block.speedup_both() # increment both wheel speed
+              elif event.key == pygame.K_t:
+                  block.slowdown_both() # decrement both wheel speed
+              elif event.key == pygame.K_ESCAPE:
+                loopExit = False
 
       block.move()
 
@@ -94,7 +145,8 @@ def gameloop():
       wall.draw()
       block.draw()
       block.draw_direction()
-
+      # text = block.message_display()
+      # screen.blit(text, (320, 240))
       clock.tick(60)
 
       pygame.display.flip()
