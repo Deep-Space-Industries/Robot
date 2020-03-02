d7c21b34ea2091cd22f0951b2dde4610c20b5ca5
Azrion <berat.cakir@outlook.com>
2020-03-02 20:06:27 +0100

    NN

--- ourSolution.py
+++ ourSolution.py
@@ -1,60 +1,80 @@
+# This code was written in pair-programming style
+# Members: Berat Cakir, Koushik Haridasyam, Zhangyi Wu
+
+# Import packages
 import numpy as np
 import random
 import math
 import matplotlib.pyplot as plt
 from matplotlib import ticker
 
-#
+# Parameters for dimensionality and distribution of positions
 mu = 0.0
 std = 5.0
 dim = 2
 scale = 0.25
-precision = 10000
 
-# Create
+# Define class individual and its variables
 class Individual:
     def __init__(self):
         self.genotype = encoder(np.random.normal(loc=mu, scale=std, size=dim))
         self.fitness = rosenbrock(decoder(self.genotype))
 
+# Create n new offsprings by m number of individuals
 def pair(bestIndividuals, n_offsprings, method):
+    # Pair individuals randomly
     if (method == "random"):
         random.shuffle(bestIndividuals)
+    # Else: pair best individuals with best individuals
     offsprings = []
     for i in range(len(bestIndividuals))[::2]:
+        # Generates n new offsprings with mutation value added
         newOffspring = Individual()
+        # Genes of parents being passed as average value
         newOffspring.genotype = (decoder(bestIndividuals[i].genotype)+decoder(bestIndividuals[i+1].genotype))/2
+        # Mutation being added to the new gene
         newOffspring.genotype = encoder(mutate(newOffspring))
         offsprings.append(newOffspring)
     return offsprings
 
+# Add mutation to genes
 def mutate(offspring):
     return offspring.genotype + np.random.normal(loc=mu*scale, scale=std*scale, size=dim)
 
+# Define class population and its variables
 class Population:
     def __init__(self, n_individuals, n_bestIndividuals, n_offsprings, n_kill, n_epochs, decreaseFactorMutation, scale=scale):
+        # Create initial population
         self.n_individuals = n_individuals
         self.individuals = [Individual() for i in range(n_individuals)]
         self.decreaseFactorMutation = decreaseFactorMutation
+        # Saving positions into history
         self.history=[]
         self.bhistory=[]
+        # Evolving for n number of epochs
         for i in range(n_epochs):
             self.history.append([decoder(k.genotype) for k in self.individuals])
             self.individuals.sort(key=lambda x: x.fitness, reverse=False)
             self.bestIndividuals = self.individuals[:n_bestIndividuals]
+            # Pairing of n individuals
             self.offsprings = pair(self.bestIndividuals, n_offsprings, "else")
+            # Calculate fitness of new offsprings
             for o in self.offsprings:
                 o.fitness = rosenbrock(decoder(o.genotype))
+            # Add new offsprings to the total population
             self.allIndividuals = self.individuals + self.offsprings
             self.allIndividuals.sort(key=lambda x: x.fitness, reverse=False)
+            # Let n number of individuals die
             self.allIndividuals = self.allIndividuals[:-n_kill]
             self.individuals = self.allIndividuals
             scale -= decreaseFactorMutation
 
-def encoder(phenotype):
+# Encodes position genes into binary format
+def encoder(phenotype, precision=10000):
     return np.array((bin(int(phenotype[0]*precision)),bin(int(phenotype[1]*precision))))
 
-def decoder(genotype):
+# Decodes binary genes into position format
+def decoder(genotype, precision=10000):
     return np.array((int(genotype[0], 2)/precision, int(genotype[1], 2)/precision))
 
 # Define Rosenbrock performance function
